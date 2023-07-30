8336b5ce94b40233224389373823c400a9c7fa9b
Arthur Neuman <arthurbneuman@gmail.com>
2023-07-30 17:23:49 -0400

    Implement some basic constructors

--- lib/SkipVector.h
+++ lib/SkipVector.h
@@ -24,17 +24,23 @@ class SkipVector {
   protected:
     typedef std::pair<offset_pair*, size_t> _calculated_offset;
 
-    size_t _m_data = 0;
-    size_t _u_data = 0;
-    size_t _m_offset = 0;
-    size_t _u_offset = 0;
+    size_t _m_data;
+    size_t _u_data;
+    size_t _m_offset;
+    size_t _u_offset;
 
-    T _data[];
-    offset_pair _offset[];
+    T* _data;
+    offset_pair* _offset;
 
     _calculated_offset _get_offset (size_t pos) const;
+    size_t _upper_power_of_two (size_t num) const;
 
   public:
+    SkipVector (size_t count = 1);
+    SkipVector (size_t count, const T& value);
+    SkipVector (const SkipVector<T>& original);
+    ~SkipVector ();
+
     T& at (size_t pos) const;
     T& operator[] (size_t pos) const;
     T& front () const;
@@ -67,8 +73,50 @@ class SkipVector {
     void resize (size_t count);
 };
 
+/**
+ * Implementation: https://stackoverflow.com/a/466278/10438436
+ */
 template <typename T>
-SkipVector<T>::_calculated_offset SkipVector<T>::_get_offset (size_t pos) const {
+size_t SkipVector<T>::_upper_power_of_two (size_t num) const {
+  --num;
+  num |= num >> 1;
+  num |= num >> 2;
+  num |= num >> 4;
+  num |= num >> 8;
+  num |= num >> 16;
+  ++num;
+
+  return num;
+}
+
+template <typename T>
+SkipVector<T>::SkipVector (size_t count) {
+  size_t rounded = _upper_power_of_two(count);
+
+  _data = new T[rounded];
+  _u_data = count;
+  _m_data = rounded;
+
+  _offset = new offset_pair[1];
+  _u_offset = 0;
+  _m_offset = 1;
+}
+
+template <typename T>
+SkipVector<T>::SkipVector (size_t count, const T& value): SkipVector(count) {
+  for (size_t i = 0; i < count; ++i) _data[i] = T(value);
+}
+
+template <typename T>
+SkipVector<T>::~SkipVector () {
+  delete[] _data;
+  delete[] _offset;
+
+  // TODO: Destruct the elements
+}
+
+template <typename T>
+typename SkipVector<T>::_calculated_offset SkipVector<T>::_get_offset (size_t pos) const {
   size_t offset = 0;
 
   offset_pair* entry = _offset;
@@ -105,7 +153,7 @@ T* SkipVector<T>::data () const {
 }
 
 template <typename T>
-SkipVector<T>::offset_pair* SkipVector<T>::offset () const {
+typename SkipVector<T>::offset_pair* SkipVector<T>::offset () const {
   return _offset;
 }
 
